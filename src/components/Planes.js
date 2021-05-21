e5ea4745eea382dfc9fa521e5c66de9aa97c8a69
luism321 <80435105+luism321@users.noreply.github.com>
2021-05-21 07:58:36 -0400

    menu acomodado

--- src/components/Planes.js
+++ src/components/Planes.js
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useHistory, Link } from "react-router-dom";
 import './Dashboard.css';
-// import { faHome, faUser, faAmbulance, faUserEdit, faClinicMedical, faPowerOff, faBars, faBell, faCommentAlt, faSmile, faGift, faIdBadge, faCalendar, faFunnelDollar } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { Alert } from "react-bootstrap"
+import { faHome, faUser, faAmbulance, faUserEdit, faClinicMedical, faPowerOff, faBars, faBell, faCommentAlt, faSmile, faGift, faIdBadge, faCalendar, faFunnelDollar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Alert } from "react-bootstrap"
 import { db } from "../firebase"
 import firebase from "../firebase"
 import moment from 'moment';
@@ -107,6 +107,97 @@ export default function Planes() {
 
     return (
         <div>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <input type="checkbox" id="sidebar-toggle" />
+
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <h3 className="brand">
+            <span className="ti-unlink"></span>
+            <span ><div id="ig" /></span>
+          </h3>
+          <label htmlFor="sidebar-toggle"><FontAwesomeIcon icon={faBars} /></label>
+        </div>
+
+        <div className="sidebar-menu" id="sidebar">
+          <ul>
+            <li>
+              <Link to="/">
+                <a >
+                  <a><FontAwesomeIcon icon={faHome} /></a>
+                  <span>Inicio</span>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Emergencia">
+                <a >
+                  <a id="ti-face-smile"><FontAwesomeIcon icon={faAmbulance} /></a>
+                  <span id="ti-face-smile">Emergencia</span>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Directorio">
+                <a>
+                  <a className="ti-face-smile"><FontAwesomeIcon icon={faClinicMedical} /></a>
+                  <span>Directotio</span>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/MisCitas">
+                <a>
+                  <a className="ti-face-smile"><FontAwesomeIcon icon={faClinicMedical} /></a>
+                  <span>Mis citas</span>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Planes" >
+                <a>
+                  <a id="planes"><FontAwesomeIcon icon={faFunnelDollar} /></a>
+                  <span id="planes" >Planes</span>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Update">
+                <a >
+                  <a className="ti-clipboard"><FontAwesomeIcon icon={faUserEdit} /></a>
+                  <span>Editar perfil</span>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/Contactos">
+                <a>
+                  <a className="ti-clipboard"><FontAwesomeIcon icon={faUser} /></a>
+                  <span>Contactos</span>
+                </a>
+              </Link>
+            </li>
+
+            <li id="cerrar">
+              <a onClick={handleLogout}>
+                <a ><FontAwesomeIcon icon={faPowerOff} /><span >Cerrar sesíon</span></a>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+      <div className="main-content">
+        <header className="row" >
+          <div className="search-wrapper">
+          </div>
+          <div className="social-icons">
+            <span className="ti-bell"><FontAwesomeIcon icon={faBell} /></span>
+            <span className="ti-comment"><FontAwesomeIcon icon={faCommentAlt} /></span>
+            <div></div>&nbsp;{currentDatos_3.nombres}
+          </div>
+        </header>
+        <main>
             {planes.length != 0 ? (
                 planes.map(array => (
                     <div>
@@ -172,7 +263,6 @@ export default function Planes() {
                             <div>
                                 <h1 className="mb-5" >$10</h1>
                             </div>
-
                             <div className="mb-3">
                                 5% de descuento en las siguientes 4 consultas y/o emergencias.
                             </div>
@@ -217,7 +307,6 @@ export default function Planes() {
                             <div>
                                 <h1 className="mb-5" >$20</h1>
                             </div>
-
                             <div className="mb-3">
                                 7% de descuento en las siguientes 12 consultas y/o emergencias.
                             </div>
@@ -262,7 +351,6 @@ export default function Planes() {
                             <div>
                                 <h1 className="mb-5" >$30</h1>
                             </div>
-
                             <div className="mb-3">
                                 10% de descuento en las siguientes 24 consultas y/o emergencias.
                             </div>
@@ -297,11 +385,11 @@ export default function Planes() {
                                 />
                             </div>
                         </div>
-
                     </div>
                 </div>
-
             </div> */}
+            </main>
+            </div>
         </div>
     )
 }
